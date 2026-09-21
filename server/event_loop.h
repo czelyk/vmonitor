d481784451184cea00135b0c9fffd6077421375c
Ahmet Çelik <111885862+czelyk@users.noreply.github.com>
2026-09-21 12:18:01 +0300

    Modify event loop functions for driver integration
    Updated event_loop_run to accept driver_fd and added event_loop_get_latest_sample function.

--- server/event_loop.h
+++ server/event_loop.h
@@ -1,6 +1,26 @@
 #ifndef EVENT_LOOP_H
 #define EVENT_LOOP_H
 
-int event_loop_run(int server_fd);
+#include "../include/vmonitor_uapi.h"
+
+/*
+ * Run the single-threaded epoll server.
+ *
+ * server_fd:
+ *     non-blocking TCP listening socket
+ *
+ * driver_fd:
+ *     non-blocking /dev/vmonitor descriptor
+ */
+int event_loop_run(int server_fd, int driver_fd);
+
+/*
+ * Returns the most recently consumed driver sample.
+ *
+ * Returns:
+ *   0  -> success
+ *  -1  -> no sample available / invalid argument
+ */
+int event_loop_get_latest_sample(struct vmonitor_sample *out);
 
 #endif
